092339016fc202a72901a30cd06e42c40394f01b
Administrator <Administrator@DESKTOP-L3KQ5AN>
2019-09-19 15:59:02 +0900

    second commit

--- SpringBootWithReact/src/main/java/com/example/demo/course/CoursesHardcodedService.java
+++ SpringBootWithReact/src/main/java/com/example/demo/course/CoursesHardcodedService.java
@@ -44,4 +44,15 @@ public class CoursesHardcodedService {
 
 	    return null;
 	  }
+  
+  public Course save(Course course) {
+	  if (course.getId() == -1 || course.getId() == 0) {
+	    course.setId(++idCounter);
+	    courses.add(course);
+	  } else {
+	    deleteById(course.getId());
+	    courses.add(course);
+	  }
+	  return course;
+	}
 }
