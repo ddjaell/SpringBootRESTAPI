6730f3ee6eae9a4dfea8e22e3d419c8357dab7ac
Administrator <Administrator@DESKTOP-L3KQ5AN>
2019-09-20 12:59:58 +0900

    third commit

--- SpringBootWithReact/src/main/java/com/example/demo/DemoApplication.java
+++ SpringBootWithReact/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
